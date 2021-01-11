5aef394c846deee70300ae3a802373df6358ab6d
marianastoiovici <56549060+marianastoiovici@users.noreply.github.com>
2021-01-11 21:04:41 +0000

    Update exercise3.cpp
    Update the program to prevent the user from continuing if the room sizes are not numeric.

--- exercise3.cpp
+++ exercise3.cpp
@@ -4,30 +4,45 @@
 #include <iostream>
 using namespace std;
 
+bool isNumeric(string str) {
+   for (int i = 0; i < str.length(); i++)
+      if (isdigit(str[i]) == false)
+         return false; //when one non numeric value is found, return false
+      return true;
+}
+
 int main() {
   const float conversionFactor = 0.3048; 
+  string length;
+  string width;
   float feetLength;
   float feetWidth;
   float metersLength;
   float metersWidth;
-  double feetArea;
-  double metersArea;
+  float feetArea;
+  float metersArea;
 
   cout << "What is the length of the room in feet? ";
-  cin >> feetLength;
+  cin >> length;
 
   cout << "What is the width of the room in feet? ";
-  cin >> feetWidth;
+  cin >> width;
 
-  metersLength = feetLength * conversionFactor;
-  metersWidth = feetWidth * conversionFactor;
-  feetArea = feetLength * feetWidth;
-  metersArea = metersLength * metersWidth;
+  // if user input is not numeric, stop the program
+  if ((isNumeric(length) == true) || (isNumeric(width) == true)) {
+    feetLength = stof(length); // std library's method to cast from a numeric string to a float.
+    metersLength = feetLength * conversionFactor;
+    feetWidth = stof(width);
+    metersWidth = feetWidth * conversionFactor;
+    feetArea = feetLength * feetWidth;
+    metersArea = metersLength * metersWidth;
 
-  cout << "\n\n" << "Your room’s dimension is " <<  feetLength << " by " << feetWidth << " feet, the area is: " <<endl;
+    cout << "\n\n" << "Your room’s dimension is " <<  feetLength << " by " << feetWidth << " feet, the area is: " <<endl;
 
-  cout << feetArea << " square foot" << endl;
-  cout << metersArea << " square meters" << endl;
-
+    cout << feetArea << " square foot" << endl;
+    cout << metersArea << " square meters" << endl;
+  } else {
+    return 0;
+  }
   return 0;
 }
